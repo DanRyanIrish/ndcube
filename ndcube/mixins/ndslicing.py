215826bf41c2bb83fe6c8c134b90b12c65c67a3d
Kris Stern <kakirastern@users.noreply.github.com>
2019-03-06 21:38:56 +0800

    Correct error concatenating two lists due to presence of NoneTypes

--- ndcube/mixins/ndslicing.py
+++ ndcube/mixins/ndslicing.py
@@ -45,7 +45,7 @@ class NDCubeSlicingMixin(NDSlicingMixin):
         wcs, missing_axis, dropped_coords = self._slice_wcs_missing_axis(item)
         kwargs['wcs'] = wcs
         kwargs['missing_axis'] = missing_axis
-        kwargs['extra_coords'] = self._slice_extra_coords(item, missing_axis) + dropped_coords
+        kwargs['extra_coords'] = self._slice_extra_coords(item, missing_axis).extend(dropped_coords)
 
         return kwargs
 
