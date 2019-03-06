98095afb51b9314057a6c9dd760405117530596c
Kris Stern <kakirastern@users.noreply.github.com>
2019-03-06 22:05:51 +0800

    Use another approach to concatenate to get extra_coords

--- ndcube/mixins/ndslicing.py
+++ ndcube/mixins/ndslicing.py
@@ -45,10 +45,8 @@ class NDCubeSlicingMixin(NDSlicingMixin):
         wcs, missing_axis, dropped_coords = self._slice_wcs_missing_axis(item)
         kwargs['wcs'] = wcs
         kwargs['missing_axis'] = missing_axis
-        kwargs['extra_1'] = self._slice_extra_coords(item, missing_axis)
-        kwargs['extra_2'] = dropped_coords
-        # kwargs['extra_coords'] = []
-        
+        kwargs['extra_coords'] = list(self._slice_extra_coords(item, missing_axis)) + list(dropped_coords)
+
         return kwargs
 
     def _slice_wcs_missing_axis(self, item):
