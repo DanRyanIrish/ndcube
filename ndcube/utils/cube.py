92cd4804d2095d5b1d410908e49a27917a61e9f7
Kris Stern <kakirastern@gmail.com>
2019-04-24 12:50:13 +0800

    Further debug cube.py

--- ndcube/utils/cube.py
+++ ndcube/utils/cube.py
@@ -93,12 +93,13 @@ def _format_input_extra_coords_to_extra_coords_wcs_axis(extra_coords, missing_ax
         # of coord is same is data axis to which is corresponds.
         if coord[1] is not None:
             if not missing_axis[::-1][coord[1]]:
-                if isinstance(coord[2], list):
-                    if len(coord[2]) != data_shape[coord[1]]:
-                        raise ValueError(coord_len_error.format(coord[0], len(coord[2]), data_shape[coord[1]]))
-                else:
-                    if data_shape[coord[1]] != 1:
-                        raise ValueError(coord_len_error.format(coord[0], 1, data_shape[coord[1]]))
+                if isinstance(coord[2], (list, tuple)):
+                    try:
+                        if len(coord[2]) != data_shape[coord[1]]:
+                            raise ValueError(coord_len_error.format(coord[0], len(coord[2]), data_shape(coord[1])))
+                    except:
+                        if data_shape[coord[1]] != 1:
+                            raise ValueError(coord_len_error.format(coord[0], 1, data_shape[coord[1]]))
         # Determine wcs axis corresponding to data axis of coord
         extra_coords_wcs_axis[coord[0]] = {
             "wcs axis": data_axis_to_wcs_axis(coord[1], missing_axis),
